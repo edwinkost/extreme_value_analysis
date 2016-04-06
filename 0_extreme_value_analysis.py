b0bc6df144b9a142580aa6d7a61a8ca18371cd73
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-04-06 14:51:26 +0200

    First test.

--- 0_extreme_value_analysis.py
+++ 0_extreme_value_analysis.py
@@ -32,7 +32,7 @@ start_year = int(sys.argv[3]) ; print(start_year)
 end_year   = int(sys.argv[4]) ; print(end_year)
 
 # output directory: 
-output_directory =  "/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/cartesius/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_4LCs_edwin_parameter_set_kinematic_wave/daily/maximum/" 
+output_directory = input_directory + "/maximum/" + str(start_year) + "to" + str(end_year) + "/"
 cleanOutputDir = False
 if cleanOutputDir:
     if os.path.exists(output_directory): shutil.rmtree(output_directory)
