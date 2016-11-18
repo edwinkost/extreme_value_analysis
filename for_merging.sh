4be35477f8f921cadd46f53e2740d85dd401ed9e
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-11-18 13:53:51 +0100

    Get the maximum events in 1960-1999 from the run using gfdl-esm2m for the hydrological year 1

---
+++ for_merging.sh
@@ -0,0 +1,7 @@
+
+cdo mergetime channelStorage_monthMax_output_*.nc    channelStorage_monthMax_output_1951-01-31_to_2005-12-31.nc &
+cdo mergetime discharge_monthAvg_output_*.nc         discharge_monthAvg_output_1951-01-31_to_2005-12-31.nc &
+cdo mergetime dynamicFracWat_monthMax_output_*.nc    dynamicFracWat_monthMax_output_1951-01-31_to_2005-12-31.nc &
+cdo mergetime floodDepth_monthMax_output_*.nc        floodDepth_monthMax_output_1951-01-31_to_2005-12-31.nc &
+cdo mergetime floodVolume_monthMax_output_*.nc       floodVolume_monthMax_output_1951-01-31_to_2005-12-31.nc &
+cdo mergetime surfaceWaterLevel_monthMax_output_*.nc surfaceWaterLevel_monthMax_output_1951-01-31_to_2005-12-31.nc &
