3eb22e932dceda06470b3e583b00ed988ccd84d3
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-11-18 11:50:45 +0100

    Get maximum events for the hydrological year 2

--- 0_get_maximum_events.py
+++ 0_get_maximum_events.py
@@ -30,7 +30,7 @@ input_files['dynamicFracWatMonthMax'] = input_files['folder'] + "/dynamicFracWat
 input_files['floodVolumeMonthMax']    = input_files['folder'] + "/floodVolume_monthMax_output_1958-01-31_to_2001-12-31.nc"                                       # unit: m3
 
 # type of hydrological year
-type_of_hydrological_year = 1         # hydrological year 1: October to September 
+type_of_hydrological_year = 2         # hydrological year 1: October to September 
 # - number of months to be shifted
 num_of_shift_month = 9
 if type_of_hydrological_year == 2:    # hydrological year 2: July to June
