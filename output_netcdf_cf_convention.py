4c1e60d4ae65b78a603dfc3b89d744054284aa83
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-11-26 22:37:09 +0100

    still in progress

--- output_netcdf_cf_convention.py
+++ output_netcdf_cf_convention.py
@@ -139,7 +139,7 @@ class OutputNetCDF():
         var = rootgrp.createVariable(shortVarName, 'f4', ('time', 'lat', 'lon',), fill_value = vos.MV, zlib = self.zlib)
         var.standard_name = shortVarName
         var.long_name = longVarName
-        var.comment = comment
+        #var.comment = comment
         
         # variable unit
         var.units = varUnit
