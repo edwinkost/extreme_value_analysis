20894b442c3ad0c3ffb3943ed6c5d000f466f5e6
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-11-27 10:42:43 +0100

    still in progress

--- glofris_postprocess_edwin_modified.py
+++ glofris_postprocess_edwin_modified.py
@@ -529,7 +529,7 @@ def get_gumbel_parameters(input_data):
         # return zero probability, location and scale parameters
         
     # input data    
-    flvol = input_data
+    flvol = input_data[:,:,:].copy()
     
     print flvol 
     
