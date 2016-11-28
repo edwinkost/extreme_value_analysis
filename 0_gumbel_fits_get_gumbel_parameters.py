d8e816ebbff3116e88bb4bbb729d74aaca97e197
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-11-28 23:06:24 +0100

    test

--- 0_gumbel_fits_get_gumbel_parameters.py
+++ 0_gumbel_fits_get_gumbel_parameters.py
@@ -164,7 +164,7 @@ for var_name in ['channelStorage', 'floodVolume', 'dynamicFracWat']:
         else:
             str_row = end_row
         #~ end_row = str_row + number_of_rows / n_cores
-        end_row = str_row + 3
+        end_row = str_row + 150
         #
         # put the input in a dictionary
         input_data = {}
