04e2c36b74b408203a38f47f83b3f6a201d6292c
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-12-03 00:23:07 +0100

    Post-processing for historical gfdl.

--- 2_merge_two_hydrological_year_result.py
+++ 2_merge_two_hydrological_year_result.py
@@ -59,7 +59,7 @@ output_files                    = {}
 #~ # - WATCH historical
 #~ output_files['folder']       = "/scratch-shared/edwinsut/flood_analyzer_analysis/maximum_events_merged/watch_1960-1999/"
 # - gfdl-esm2m historical
-output_files['folder']          = "/scratch-shared/edwinhs/flood_analyzer_analysis/maximum_events_merged/gfdl-esm2m_1960-1999/"
+output_files['folder']          = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/gfdl-esm2m_1960-1999/"
 #
 try:
     os.makedirs(output_files['folder'])
