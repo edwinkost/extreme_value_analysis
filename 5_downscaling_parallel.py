552ddaa3182e17df4ef19cce3ea2ba62c8870f58
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-12-07 11:06:36 +0100

    Final fixing.

--- 5_downscaling_parallel.py
+++ 5_downscaling_parallel.py
@@ -38,6 +38,8 @@ logger.info(msg)
 #
 # - the first part: the relative big ones
 clone_codes  = ["M17","M19","M26","M13","M18","M20","M05","M03","M21","M46","M27","M49","M16","M44","M52","M25","M09","M08","M11","M42","M12","M39"]
+# - and plus two of the smallest ones 
+clone_codes += ["M30","M29"]
 msg = "Run the downscaling scripts for " + str(clone_codes)
 logger.info(msg)
 i_clone = 0
@@ -60,9 +62,8 @@ while status == False:
    status = vos.check_downscaling_status(general_output_folder, clone_codes)
 #
 #
-# - the second part: # the relative small ones
-clone_codes = ["M07","M15","M38","M48","M40","M41","M22","M14","M23","M51","M04","M06","M10","M02","M45","M35","M47","M50","M24","M01","M36","M53","M33","M43","M34","M37","M31","M32","M28","M30","M29"]
-msg = "Run the downscaling scripts for " + str(clone_codes)
+# - the second part: # the relative small ones minus two of the smallest ones
+clone_codes = ["M07","M15","M38","M48","M40","M41","M22","M14","M23","M51","M04","M06","M10","M02","M45","M35","M47","M50","M24","M01","M36","M53","M33","M43","M34","M37","M31","M32","M28"]
 msg = "Run the downscaling scripts for " + str(clone_codes)
 logger.info(msg)
 i_clone = 0
