52a29524c17c9a4231d5b30ce13994e664919f95
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-12-10 21:47:41 +0100

    Post-processing for ipsl-cm5a-lr: 2_merge_two_hydrological_year_result.py

--- 2_merge_two_hydrological_year_result.py
+++ 2_merge_two_hydrological_year_result.py
@@ -37,8 +37,10 @@ input_files                     = {}
 #~ input_files['folder']        = "/scratch-shared/edwinsut/flood_analyzer_analysis/maximum_events/watch_1960-1999/"
 #~ # - gfdl-esm2m historical
 #~ input_files['folder']        = "/scratch-shared/edwinsut/flood_analyzer_analysis/maximum_events/gfdl-esm2m_1960-1999/"
-# - hadgem2-es historical
-input_files['folder']           = "/scratch-shared/edwinsut/flood_analyzer_analysis/maximum_events/hadgem2-es_1960-1999/"
+#~ # - hadgem2-es historical
+#~ input_files['folder']        = "/scratch-shared/edwinsut/flood_analyzer_analysis/maximum_events/hadgem2-es_1960-1999/"
+# - ipsl-cm5a-lr historical
+input_files['folder']           = "/scratch-shared/edwinsut/flood_analyzer_analysis/maximum_events/ipsl-cm5a-lr_1960-1999/"
 #
 # general input files
 # - clone map
@@ -64,8 +66,10 @@ output_files                    = {}
 #~ output_files['folder']       = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/watch_1960-1999/"
 #~ # - gfdl-esm2m historical
 #~ output_files['folder']       = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/gfdl-esm2m_1960-1999/"
-# - hadgem2-es historical
-output_files['folder']          = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/hadgem2-es_1960-1999/"
+#~ # - hadgem2-es historical
+#~ output_files['folder']       = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/hadgem2-es_1960-1999/"
+# - ipsl-cm5a-lr historical
+output_files['folder']          = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/ipsl-cm5a-lr_1960-1999/"
 #
 try:
     os.makedirs(output_files['folder'])
