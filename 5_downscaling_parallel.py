f66cf1822a5885e471e2640cd6ff2620339bfa98
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-12-10 22:34:30 +0100

    Post-processing for gfdl-esm2m: 5_downscaling_parallel.py

--- 5_downscaling_parallel.py
+++ 5_downscaling_parallel.py
@@ -14,13 +14,20 @@ import virtualOS as vos
 import logging
 logger = logging.getLogger(__name__)
 
+
 # input folder that contain extreme values (in pcraster format):
-# - WATCH historical
-input_folder          = "/scratch-shared/edwinhs-last/flood_analyzer_output/extreme_values/watch_1960-1999/"
+#~ # - WATCH historical
+#~ input_folder       = "/scratch-shared/edwinhs-last/flood_analyzer_output/extreme_values/watch_1960-1999/"
+# - gfdl-esm2m historical
+input_folder          = "/scratch-shared/edwinhs-last/flood_analyzer_output/extreme_values/gfdl-esm2m_1960-1999/"
+
 
 # output folder
-# - WATCH historical
-general_output_folder = "/scratch-shared/edwinhs-last/flood_analyzer_output/inundation_downscaled/watch_1960-1999/"
+#~ # - WATCH historical
+#~ general_output_folder = "/scratch-shared/edwinhs-last/flood_analyzer_output/inundation_downscaled/watch_1960-1999/"
+# - gfdl-esm2m historical
+general_output_folder    = "/scratch-shared/edwinhs-last/flood_analyzer_output/inundation_downscaled/gfdl-esm2m_1960-1999/"
+
 
 # clean any files exists on the ouput directory (this can be done for global runs)
 clean_previous_output = True
