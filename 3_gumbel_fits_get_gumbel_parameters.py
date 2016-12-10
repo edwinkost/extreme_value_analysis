9f83d1183721258f9128f586b5af77122c5aec67
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-12-10 17:13:12 +0100

    Post-processing for GFDL-ESM2M: 3_gumbel_fits_get_gumbel_parameters.py

--- 3_gumbel_fits_get_gumbel_parameters.py
+++ 3_gumbel_fits_get_gumbel_parameters.py
@@ -34,10 +34,11 @@ logger = logging.getLogger(__name__)
 input_files                    = {}
 #
 # The annual flood maxima based on the PCR-GLOBWB 5 arcmin results:
-# - WATCH historical
-input_files['folder']    = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/watch_1960-1999/"
-#~ # - gfdl-esm2m historical
-#~ input_files['folder'] = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/gfdl-esm2m_1960-1999/"
+#
+#~ # - WATCH historical
+#~ input_files['folder']    = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/watch_1960-1999/"
+# - gfdl-esm2m historical
+input_files['folder']       = "/scratch-shared/edwinhs-last/flood_analyzer_output/maximum_events_merged/gfdl-esm2m_1960-1999/"
 #
 input_files['file_name'] = {}
 input_files['file_name']['channelStorage'] = input_files['folder'] + "/" + "channel_storage_annual_flood_maxima.nc" 
@@ -61,11 +62,13 @@ end_year = 1999
 
 # output files
 output_files                   = {}
-#~ # output folder
-# - WATCH historical
-output_files['folder']         = "/scratch-shared/edwinhs-last/flood_analyzer_output/gumbel_fits/watch_1960-1999/"
-#~ # - gfdl-esm2m historical
-#~ output_files['folder']      = "/scratch-shared/edwinhs-last/flood_analyzer_output/gumbel_fits/gfdl-esm2m_1960-1999/"
+
+# output folder
+#
+#~ # - WATCH historical
+#~ output_files['folder']      = "/scratch-shared/edwinhs-last/flood_analyzer_output/gumbel_fits/watch_1960-1999/"
+# - gfdl-esm2m historical
+output_files['folder']         = "/scratch-shared/edwinhs-last/flood_analyzer_output/gumbel_fits/gfdl-esm2m_1960-1999/"
 #
 try:
     os.makedirs(output_files['folder'])
