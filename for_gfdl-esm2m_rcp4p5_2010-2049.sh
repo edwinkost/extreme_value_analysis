9e2fb886925ba9c00e787ad71fe0959d9b394ed5
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-12-30 02:28:52 +0100

    Run 1a_get_maximum_events.py for gfdl-esm2m_rcp4p5_2010-2049

---
+++ for_gfdl-esm2m_rcp4p5_2010-2049.sh
@@ -0,0 +1,5 @@
+
+git pull
+
+python 1a_get_maximum_events.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_gfdl-esm2m/no_correction/rcp4p5/merged_2006-2099/ 1 /projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_rcp4p5_runs/maximum_events/gfdl-esm2m_2010-2049/ &
+python 1a_get_maximum_events.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_gfdl-esm2m/no_correction/rcp4p5/merged_2006-2099/ 2 /projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_rcp4p5_runs/maximum_events/gfdl-esm2m_2010-2049/ &
