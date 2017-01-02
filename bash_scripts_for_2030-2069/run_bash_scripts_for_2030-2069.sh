9b2a8795080dc205b89552616f381680c80d7637
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-01-02 03:10:24 +0100

    Post-processing for 2030-2069 (2050)

---
+++ bash_scripts_for_2030-2069/run_bash_scripts_for_2030-2069.sh
@@ -0,0 +1,16 @@
+
+bash bash_scripts_for_2030-2069/post-processing_for_gfdl-esm2m_rcp4p5_2030-2069.sh &
+bash bash_scripts_for_2030-2069/post-processing_for_gfdl-esm2m_rcp8p5_2030-2069.sh &
+wait 
+
+bash bash_scripts_for_2030-2069/post-processing_for_hadgem2-es_rcp4p5_2030-2069.sh   &
+bash bash_scripts_for_2030-2069/post-processing_for_hadgem2-es_rcp8p5_2030-2069.sh   &
+bash bash_scripts_for_2030-2069/post-processing_for_ipsl-cm5a-lr_rcp4p5_2030-2069.sh &
+bash bash_scripts_for_2030-2069/post-processing_for_ipsl-cm5a-lr_rcp8p5_2030-2069.sh &
+wait
+
+bash bash_scripts_for_2030-2069/post-processing_for_miroc-esm-chem_rcp4p5_2030-2069.sh &
+bash bash_scripts_for_2030-2069/post-processing_for_miroc-esm-chem_rcp8p5_2030-2069.sh &
+bash bash_scripts_for_2030-2069/post-processing_for_noresm1-m_rcp4p5_2030-2069.sh &
+bash bash_scripts_for_2030-2069/post-processing_for_noresm1-m_rcp8p5_2030-2069.sh &
+wait
