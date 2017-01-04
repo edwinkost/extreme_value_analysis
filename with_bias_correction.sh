04de82af475416aebad6458b465397e44979d8d1
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-01-04 17:23:31 +0100

    Make the downscaling.py more flexible.

--- with_bias_correction.sh
+++ with_bias_correction.sh
@@ -3,5 +3,5 @@ python 5ab_gumbel_fits_apply_gumbel_parameters_for_inundation_with_bias_correcti
  
 python 5bb_gumbel_fits_apply_gumbel_parameters_for_surface_water_level_with_bias_correction.py 2010 2049 /scratch-shared/edwinhs/bias_correction_test/input/rcp4p5/gumbel_fits_surface_water_level/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/input/historical/gumbel_fits_surface_water_level/gfdl-esm2m_1960-1999/ /scratch-shared/edwinhs/bias_correction_test/output/extreme_values_surface_water_level_bias_corrected/gfdl-esm2m_2010-2049/
 
-python 6_downscaling_parallel.py /scratch-shared/edwinhs/bias_correction_test/output/extreme_values_bias_corrected/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/output/extreme_values_bias_corrected/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/output/inundation_downscaled_bias_corrected/gfdl-esm2m_2010-2049/ bias_corrected
+python 6_downscaling_parallel.py /scratch-shared/edwinhs/bias_correction_test/output/extreme_values_bias_corrected/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/output/inundation_downscaled_bias_corrected/gfdl-esm2m_2010-2049/ bias_corrected
 
