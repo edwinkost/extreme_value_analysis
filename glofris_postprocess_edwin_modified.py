448b6f33ce8c899d3d17cb30803835c6dc59cd7d
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-01-04 10:40:04 +0100

    Test for inundation.

--- glofris_postprocess_edwin_modified.py
+++ glofris_postprocess_edwin_modified.py
@@ -786,10 +786,10 @@ def get_return_period_gumbel(p_zero_in_pcraster, loc_in_pcraster, scale_in_pcras
     #~ p_residual = np.minimum(np.maximum(np.exp(-np.exp(-np.longdouble(reduced_variate))), np.longdouble(0.0)), np.longdouble(1.0))
     p_residual = np.minimum(np.maximum(np.exp(-np.exp(-np.longdouble(reduced_variate))), 0.0), 1.0)
 
-    print np.nanmin(p_residual)
-    print np.nanmax(p_residual)
-    print np.amin(p_residual)
-    print np.amax(p_residual)
+    #~ print np.nanmin(p_residual)
+    #~ print np.nanmax(p_residual)
+    #~ print np.amin(p_residual)
+    #~ print np.amax(p_residual)
 
     # transform from non-zero only distribution to zero-included distribution
     p = np.minimum(np.maximum(p_residual*(1.0 - p_zero) + p_zero, p_zero), max_p)  # never larger than max_p # 
