331ff3a38146472311d6bf55767a685c9d116c31
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-01-05 16:28:23 +0100

    start apply_gumbel_and_downscaling_for_hadgem2-es_rcp4p5_2010-2049.sh

---
+++ correcting_name.sh
@@ -0,0 +1,7 @@
+
+mv gfdl-esm2m_2060-2069     gfdl-esm2m_2030-2069
+mv hadgem2-es_2060-2069     hadgem2-es_2030-2069
+mv ipsl-cm5a-lr_2060-2069   ipsl-cm5a-lr_2030-2069
+mv miroc-esm-chem_2060-2069 miroc-esm-chem_2030-2069
+mv noresm1-m_2060-2069      noresm1-m_2030-2069
+
