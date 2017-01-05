88c3db1d1a88313584899c860fa4df7c05ecf805
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-01-05 07:31:43 +0100

    Test for rcp8p5

--- bash_scripts_for_2010-2049_with_bias_correction/apply_gumbel_and_downscaling_for_gfdl-esm2m_rcp4p5_2010-2049.sh
+++ bash_scripts_for_2010-2049_with_bias_correction/apply_gumbel_and_downscaling_for_gfdl-esm2m_rcp4p5_2010-2049.sh
@@ -7,13 +7,13 @@
 # calculate gumbel parameters for surface water level
 
 # apply gumbel fits with bias correction for inundation
-python 5ab_gumbel_fits_apply_gumbel_parameters_for_inundation_with_bias_correction.py 2010 2049 /scratch-shared/edwinhs/bias_correction_test/input/rcp4p5/gumbel_fits/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/input/historical/gumbel_fits/gfdl-esm2m_1960-1999/ /scratch-shared/edwinhs/bias_correction_test/output/extreme_values_bias_corrected/gfdl-esm2m_2010-2049/
+python 5ab_gumbel_fits_apply_gumbel_parameters_for_inundation_with_bias_correction.py 2010 2049 /scratch-shared/edwinhs/bias_correction_test/input/rcp4p5/gumbel_fits/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/input/historical/gumbel_fits/gfdl-esm2m_1960-1999/ /scratch-shared/edwinhs/bias_correction_test/output/rcp4p5/extreme_values_bias_corrected/gfdl-esm2m_2010-2049/
  
 # apply gumbel fits for surface water level for surface water level
-python 5bb_gumbel_fits_apply_gumbel_parameters_for_surface_water_level_with_bias_correction.py 2010 2049 /scratch-shared/edwinhs/bias_correction_test/input/rcp4p5/gumbel_fits_surface_water_level/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/input/historical/gumbel_fits_surface_water_level/gfdl-esm2m_1960-1999/ /scratch-shared/edwinhs/bias_correction_test/output/extreme_values_surface_water_level_bias_corrected/gfdl-esm2m_2010-2049/
+python 5bb_gumbel_fits_apply_gumbel_parameters_for_surface_water_level_with_bias_correction.py 2010 2049 /scratch-shared/edwinhs/bias_correction_test/input/rcp4p5/gumbel_fits_surface_water_level/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/input/historical/gumbel_fits_surface_water_level/gfdl-esm2m_1960-1999/ /scratch-shared/edwinhs/bias_correction_test/output/rcp4p5/extreme_values_surface_water_level_bias_corrected/gfdl-esm2m_2010-2049/
 
 # downscaling inundation
-python 6_downscaling_parallel.py /scratch-shared/edwinhs/bias_correction_test/output/extreme_values_bias_corrected/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/output/inundation_downscaled_bias_corrected/gfdl-esm2m_2010-2049/ bias_corrected
+python 6_downscaling_parallel.py /scratch-shared/edwinhs/bias_correction_test/output/rcp4p5/extreme_values_bias_corrected/gfdl-esm2m_2010-2049/ /scratch-shared/edwinhs/bias_correction_test/output/rcp4p5/inundation_downscaled_bias_corrected/gfdl-esm2m_2010-2049/ bias_corrected
 
 # merging the downscaled maps
-python 7_merging_downscaled_maps.py /scratch-shared/edwinhs/bias_correction_test/output/inundation_downscaled_bias_corrected/gfdl-esm2m_2010-2049/ inunriver_rcp4p5_gfdl-esm2m_2010-2049.nc
+python 7_merging_downscaled_maps.py /scratch-shared/edwinhs/bias_correction_test/output/rcp4p5/inundation_downscaled_bias_corrected/gfdl-esm2m_2010-2049/ inunriver_rcp4p5_gfdl-esm2m_2010-2049.nc
