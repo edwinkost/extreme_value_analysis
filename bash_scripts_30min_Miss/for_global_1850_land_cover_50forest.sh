36f14f60d8d8b8890cb787a7ed8f1b70518b45c8
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-01-28 10:22:41 +0100

    For Global_1850_landCover_50Forest

---
+++ bash_scripts_30min_Miss/for_global_1850_land_cover_50forest.sh
@@ -0,0 +1,22 @@
+#~ edwinhs@tcn725:/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage$ ls -lah
+#~ total 24K
+#~ drwxr-sr-x 6 edwinhs dfguu 4.0K Jan 25 14:55 .
+#~ drwxr-sr-x 4 edwinhs dfguu 4.0K Jan 27 09:26 ..
+#~ drwxr-xr-x 8 edwinhs dfguu 4.0K Jan 25 14:55 Global_1850_landCover
+#~ drwxr-sr-x 8 edwinhs dfguu 4.0K Jan 25 14:46 Global_1850_landCover_50Forest
+#~ drwxr-sr-x 8 edwinhs dfguu 4.0K Jan 25 14:12 Global_1850_landCover_noForrest
+#~ drwxr-xr-x 8 edwinhs dfguu 4.0K Jan 25 15:03 Global_2000_landCover
+
+
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-21 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-21/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-22 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-22/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-23 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-23/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-24 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-24/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-25 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-25/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-26 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-26/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-27 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-27/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-28 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-28/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-29 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-29/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-30 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-30/ &
+python downscaling_from_specific_dates.py "/projects/0/dfguu/users/edwinhs/data/from_niko_inundation_paper/simulation_result/channelStorage/Global_1850_landCover_50Forest/netcdf/channelStorage_dailyTot.nc" "channelStorage" 1993-07-31 /scratch-shared/edwinhs/for_niko/output_folder_Global_1850_landCover_50Forest_1993-07-31/ &
+wait
