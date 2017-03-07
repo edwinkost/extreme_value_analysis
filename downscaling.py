5941c64a96af93744cb1a8683ba17412038aa462
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-03-07 11:34:46 +0100

    test again

--- downscaling.py
+++ downscaling.py
@@ -105,6 +105,7 @@ if map_type_name == "channel_storage.map":
 front_name = ""
 if type_of_files != "normal": front_name = type_of_files + "_"
 for i_file in range(0, len(file_names)):
+    file_name = file_names[i_file]
     complete_file_name = input_folder + "/" + front_name + file_name[i_file]
     extreme_value_map = pcr.cover(
                         vos.readPCRmapClone(complete_file_name, \
