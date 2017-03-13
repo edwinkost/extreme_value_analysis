ddf5023747ea18256b16212257a4cc8a421796d3
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-03-13 16:53:28 +0100

    test using HESSEL results

---
+++ bash_scripts_for_testing_with_watch/downscaling_watch_baseline_1960-1999_hessel_results.sh
@@ -0,0 +1,4 @@
+
+# downscaling inundation
+python 6_downscaling_parallel_hessel_results.py /nfs/home4/edwinhs/github/edwinkost/extreme_value_analysis/previous_results/ /scratch-shared/edwinhs/debugging_downscaling/using_previous_result/ normal HESSEL_RESULT
+
