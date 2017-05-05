e5ff3159f795b4e7f2096ae9e6d9803bc88215ee
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-05-05 20:59:38 +0200

    downscaling using strahler order 8

--- downscaling.py
+++ downscaling.py
@@ -343,10 +343,11 @@ logger.info(msg)
 stream_order_map = pcr.streamorder(ldd_map_high_resolution)
 #
 # strahler order option
-strahler_order_used    = 4
+#~ strahler_order_used = 4
 #~ strahler_order_used = 5
 #~ strahler_order_used = 6
 #~ strahler_order_used = 7
+strahler_order_used    = 8
 #
 # TODO: ignore smaller rivers (< 10 m)
 #
