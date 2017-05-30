d35e4bd0dcb5de2b57fad0b90d2caa73c2cc896f
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-05-30 09:50:11 +0200

    downscaling for historical runs HadGEM2-ES

---
+++ 1_downscaling_historical_runs/downscaling_hadgem2-es_baseline_1960-1999_using_strahler_order_6.sh
@@ -0,0 +1,6 @@
+
+# Using strahler order 6
+# - downscaling inundation
+python 6_downscaling_parallel.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/extreme_values/hadgem2-es_1960-1999/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_historical/hadgem2-es_1960-1999_using_strahler_order_6/ normal channel_storage.map 6
+# - merging all downscaled maps
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_historical/hadgem2-es_1960-1999_using_strahler_order_6/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_historical/hadgem2-es_1960-1999_using_strahler_order_6/ inunriver_historical_0000HadGEM2-ES_1980 1960 1999 channel_storage.map 06
