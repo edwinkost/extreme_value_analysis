377a458dbf50f463c7dbb0314045413f0a917b2e
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-06-13 13:42:42 +0200

    restart analysis_for_historical_runs/post_processing_noresm1-m.sh

--- 0_analysis_for_historical_runs/post_processing_noresm1-m.sh
+++ 0_analysis_for_historical_runs/post_processing_noresm1-m.sh
@@ -27,7 +27,7 @@ python 4_gumbel_fits_get_gumbel_parameters.py "/scratch-shared/edwinsut/flood_an
 ###################################################################################
 
 # apply gumbel parameters without/with and with bias correction, for annual flood maxima variables: ['surfaceWaterLevel'] 
-python  5a_gumbel_fits_apply_gumbel_parameters_without_bias_correction_for_historical_and_baseline_runs.py "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/noresm1-m/1960-1999/gumbel_fits/" "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/noresm1-m/1960-1999/extreme_values/" 1960 1999 surface_water_level_historical_noresm1-m_1980 surfaceWaterLevel
+python  5a_gumbel_fits_apply_gumbel_parameters_without_bias_correction_for_historical_and_baseline_runs.py "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/noresm1-m/1960-1999/gumbel_fits/" "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/noresm1-m/1960-1999/extreme_values/" 1960 1999 surface_water_level_historical_00000NorESM1-M_1980 surfaceWaterLevel
 
 
 ###################################################################################
