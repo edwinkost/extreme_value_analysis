6998f7930d4de90c6955e53fe9cda41365301c4b
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-06-18 14:15:34 +0200

    extreme_value_analysis_rcp2p6.sh

---
+++ 0_analysis_for_rcp_runs/rcp2p6/extreme_value_analysis/run_extreme_value_analysis_rcp2p6.sh
@@ -0,0 +1,8 @@
+
+bash 0_analysis_for_rcp_runs/rcp2p6/extreme_value_analysis/extreme_value_analysis_rcp2p6_gfdl-esm2m.sh &
+bash 0_analysis_for_rcp_runs/rcp2p6/extreme_value_analysis/extreme_value_analysis_rcp2p6_hadgem2-es.sh &
+wait
+bash 0_analysis_for_rcp_runs/rcp2p6/extreme_value_analysis/extreme_value_analysis_rcp2p6_ipsl-cm5a-lr.sh & 
+bash 0_analysis_for_rcp_runs/rcp2p6/extreme_value_analysis/extreme_value_analysis_rcp2p6_miroc-esm-chem.sh &
+wait
+bash 0_analysis_for_rcp_runs/rcp2p6/extreme_value_analysis/extreme_value_analysis_rcp2p6_noresm1-m.sh
