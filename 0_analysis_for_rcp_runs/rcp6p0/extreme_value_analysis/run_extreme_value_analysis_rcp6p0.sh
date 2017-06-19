a30e781dfbb6bb4936829fea8b4e84e3c9b6c7bd
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-06-19 11:18:29 +0200

    re-run extreme value analysis for rcp6p0

---
+++ 0_analysis_for_rcp_runs/rcp6p0/extreme_value_analysis/run_extreme_value_analysis_rcp6p0.sh
@@ -0,0 +1,9 @@
+
+bash 0_analysis_for_rcp_runs/rcp6p0/extreme_value_analysis/extreme_value_analysis_rcp6p0_gfdl-esm2m.sh &
+wait
+bash 0_analysis_for_rcp_runs/rcp6p0/extreme_value_analysis/extreme_value_analysis_rcp6p0_hadgem2-es.sh &
+bash 0_analysis_for_rcp_runs/rcp6p0/extreme_value_analysis/extreme_value_analysis_rcp6p0_ipsl-cm5a-lr.sh & 
+wait
+bash 0_analysis_for_rcp_runs/rcp6p0/extreme_value_analysis/extreme_value_analysis_rcp6p0_miroc-esm-chem.sh &
+bash 0_analysis_for_rcp_runs/rcp6p0/extreme_value_analysis/extreme_value_analysis_rcp6p0_noresm1-m.sh
+wait
