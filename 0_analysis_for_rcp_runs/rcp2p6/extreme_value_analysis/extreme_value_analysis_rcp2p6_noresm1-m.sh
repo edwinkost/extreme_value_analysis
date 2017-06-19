3da930e926ea04e87aca398c16af5f61a37d5f28
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-06-19 12:56:46 +0200

    re-run extreme value analysis for rcp2p6 - with jobs

--- 0_analysis_for_rcp_runs/rcp2p6/extreme_value_analysis/extreme_value_analysis_rcp2p6_noresm1-m.sh
+++ 0_analysis_for_rcp_runs/rcp2p6/extreme_value_analysis/extreme_value_analysis_rcp2p6_noresm1-m.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                                                                                                                  
 #SBATCH -N 1                                                                                                                 
-#SBATCH -t 119:59:00                                                                                                         
+#SBATCH -t 17:00:00                                                                                                         
 #SBATCH -p normal                                                                                                            
 #SBATCH --constraint=haswell                                                                                                 
                                                                                                                              
