fab539958e13fb7a5a088cd77148f21b0d109f74
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-02-12 15:00:21 +0100

    Using ONE consistent bankfull capacity map.

---
+++ 1_downscaling_the_historical_run_WATCH-ONLY/downscaling_watch_historical_1960-1999_using_strahler_order_6.sh
@@ -0,0 +1,8 @@
+
+# Using strahler order 6
+# - downscaling inundation
+python 6_downscaling_parallel.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/extreme_values/watch_1960-1999/ /scratch-shared/edwinsut/flood_inundation_2018_02_XX/historical/watch/1960-1999/ normal channel_storage.map 6
+#
+#~ # - merging all downscaled maps
+#~ python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_historical/gfdl-esm2m_1960-1999_using_strahler_order_6/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_historical/gfdl-esm2m_1960-1999_using_strahler_order_6/ inunriver_historical_0000GFDL-ESM2M_1980 1960 1999 channel_storage.map 06
+
