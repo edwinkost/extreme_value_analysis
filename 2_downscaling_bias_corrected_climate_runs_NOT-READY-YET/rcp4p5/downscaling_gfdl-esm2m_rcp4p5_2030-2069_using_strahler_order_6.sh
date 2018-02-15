811f740ee6c8d406f110df55e10bfbc1a29a27e0
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-15 01:55:00 +0100

    Start working for downscaling the future/climate runs.

--- 2_downscaling_bias_corrected_climate_runs_NOT-READY-YET/rcp4p5/downscaling_gfdl-esm2m_rcp4p5_2030-2069_using_strahler_order_6.sh
+++
@@ -1,7 +0,0 @@
-
-# Using strahler order 6
-# - downscaling inundation
-python 6_downscaling_parallel.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/rcp4p5/bias_corrected_flood_analyzer_analysis_rcp4p5_runs_2030-2069/rcp4p5/extreme_values_bias_corrected/gfdl-esm2m_2030-2069/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp4p5/gfdl-esm2m_2030-2069_using_strahler_order_6/ bias_corrected channel_storage.map 6
-# - merging all downscaled maps
-python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp4p5/gfdl-esm2m_2030-2069_using_strahler_order_6/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp4p5/gfdl-esm2m_2030-2069_using_strahler_order_6/ inunriver_rcp4p5_0000GFDL-ESM2M_2050 2030 2069 channel_storage.map 06
-
--- 2_downscaling_bias_corrected_climate_runs_NOT-READY-YET/rcp4p5/downscaling_ipsl-cm5a-lr_rcp4p5_2010-2049_using_strahler_order_6.sh
+++
@@ -1,7 +0,0 @@
-
-# Using strahler order 6
-# - downscaling inundation
-python 6_downscaling_parallel.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/rcp4p5/bias_corrected_flood_analyzer_analysis_rcp4p5_runs_2010-2049/rcp4p5/extreme_values_bias_corrected/ipsl-cm5a-lr_2010-2049/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp4p5/ipsl-cm5a-lr_2010-2049_using_strahler_order_6/ bias_corrected channel_storage.map 6
-# - merging all downscaled maps
-python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp4p5/ipsl-cm5a-lr_2010-2049_using_strahler_order_6/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp4p5/ipsl-cm5a-lr_2010-2049_using_strahler_order_6/ inunriver_rcp4p5_00IPSL-CM5A-LR_2030 2010 2049 channel_storage.map 06
-
