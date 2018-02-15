5e4be86359cf61b7007f53e1275c3123f0a6e29d
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-15 01:04:06 +0100

    Fixing the landmask for merging.

--- 1_downscaling_the_historical_run_WATCH-ONLY/downscaling_watch_historical_1960-1999_using_strahler_order_6.sh
+++ 1_downscaling_the_historical_run_WATCH-ONLY/downscaling_watch_historical_1960-1999_using_strahler_order_6.sh
@@ -1,19 +1,22 @@
 
+STA_PERIOD=1960
+END_PERIOD=1999
+MID_PERIOD=1980
 
-#~ EXTREME_VALUE_INPUT_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/extreme_values/watch_1960-1999/
-#~ UNMERGED_MAP_OUTPUT_FOLDER=/scratch-shared/edwinsut/flood_inundation_2018_02_XX/before_merged/historical/WATCH/1960-1999/
-#~ 
-#~ TYPE_OF_EXTREME_VALUE_FILE=normal
-#~ 
-#~ 
-#~ 
-#~ # other options: using strahler order 6 to downscale channel_storage.map
-#~ 
-#~ 
-#~ # - downscaling inundation
-#~ python 6_downscaling_parallel.py $EXTREME_VALUE_INPUT_FOLDER $UNMERGED_MAP_OUTPUT_FOLDER $TYPE_OF_EXTREME_VALUE_FILE channel_storage.map 6
+NETCDF_OUTPUT_CONVENTION=inunriver_historical_000000000WATCH_$MID_PERIOD
 
+MERGED_MAP_OUTPUT_FOLDER=/scratch-shared/edwinsut/flood_inundation_2018_02_XX/merged/historical/WATCH/$STA_PERIOD-$END_PERIOD/
+
+UNMERGED_MAP_OUTPUT_FOLDER=/scratch-shared/edwinsut/flood_inundation_2018_02_XX/before_merged/historical/WATCH/$STA_PERIOD-$END_PERIOD/
+
+EXTREME_VALUE_INPUT_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/extreme_values/watch_1960-1999/
+
+TYPE_OF_EXTREME_VALUE_FILE=normal
+
+# other options: using strahler order 6 to downscale channel_storage.map
+
+# - downscaling inundation
+python 6_downscaling_parallel.py $EXTREME_VALUE_INPUT_FOLDER $UNMERGED_MAP_OUTPUT_FOLDER $TYPE_OF_EXTREME_VALUE_FILE channel_storage.map 6
 
 # - merging all downscaled maps
-python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py /scratch-shared/edwinsut/flood_inundation_2018_02_XX/before_merged/historical/WATCH/1960-1999/ /scratch-shared/edwinsut/flood_inundation_2018_02_XX/merged/historical/WATCH/1960-1999/ inunriver_historical_000000000WATCH_1980 1960 1999 channel_storage.map 06
-
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py $UNMERGED_MAP_OUTPUT_FOLDER $MERGED_MAP_OUTPUT_FOLDER $NETCDF_OUTPUT_CONVENTION $STA_PERIOD $END_PERIOD channel_storage.map 06
