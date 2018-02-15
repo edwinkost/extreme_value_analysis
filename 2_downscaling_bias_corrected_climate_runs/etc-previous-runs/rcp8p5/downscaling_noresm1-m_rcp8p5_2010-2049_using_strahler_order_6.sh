7b1094290fa13941f1617cdf6c38e295489b8503
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-15 01:55:15 +0100

    Start working for downscaling the future/climate runs.

---
+++ 2_downscaling_bias_corrected_climate_runs/etc-previous-runs/rcp8p5/downscaling_noresm1-m_rcp8p5_2010-2049_using_strahler_order_6.sh
@@ -0,0 +1,6 @@
+
+# Using strahler order 6
+# - downscaling inundation
+python 6_downscaling_parallel.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/rcp8p5/bias_corrected_flood_analyzer_analysis_rcp8p5_runs_2010-2049/rcp8p5/extreme_values_bias_corrected/noresm1-m_2010-2049/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp8p5/noresm1-m_2010-2049_using_strahler_order_6/ bias_corrected channel_storage.map 6
+# - merging all downscaled maps
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp8p5/noresm1-m_2010-2049_using_strahler_order_6/ /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2017_05_XX/downscaling_bias_corrected_climate_runs/rcp8p5/noresm1-m_2010-2049_using_strahler_order_6/ inunriver_rcp8p5_NorESM1-M_2030 2010 2049 channel_storage.map 06
