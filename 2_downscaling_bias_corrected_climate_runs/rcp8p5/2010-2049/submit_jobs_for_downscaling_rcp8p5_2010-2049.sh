17a2c184eea3dddd621cedcd3caa96c7d5222e5a
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-02-16 12:16:31 +0100

    rcp8p5 - 2010-2049 - using fcn and bash (not sbatch)

--- 2_downscaling_bias_corrected_climate_runs/rcp8p5/2010-2049/submit_jobs_for_downscaling_rcp8p5_2010-2049.sh
+++
@@ -1,6 +0,0 @@
-
-sbatch downscaling_gfdl-esm2m_rcp8p5_2010-2049.sh 
-sbatch downscaling_hadgem2-es_rcp8p5_2010-2049.sh 
-sbatch downscaling_ipsl-cm5a-lr_rcp8p5_2010-2049.sh 
-sbatch downscaling_miroc-esm-chem_rcp8p5_2010-2049.sh 
-sbatch downscaling_noresm1-m_rcp8p5_2010-2049.sh
