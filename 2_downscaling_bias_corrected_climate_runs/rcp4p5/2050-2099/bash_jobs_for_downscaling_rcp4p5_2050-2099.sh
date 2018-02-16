01da9b545fb584d52b85adaf3e491fe0579324ee
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-17 00:35:57 +0100

    rcp4p5 - 2050-2099 (2080)

--- 2_downscaling_bias_corrected_climate_runs/rcp4p5/2050-2099/bash_jobs_for_downscaling_rcp4p5_2050-2099.sh
+++ 2_downscaling_bias_corrected_climate_runs/rcp4p5/2050-2099/bash_jobs_for_downscaling_rcp4p5_2050-2099.sh
@@ -1,4 +1,6 @@
 
+cd /home/edwinsut/github/edwinkost/extreme_value_analysis/2_downscaling_bias_corrected_climate_runs/rcp4p5/2050-2099/
+
 bash downscaling_gfdl-esm2m_rcp4p5_2050-2099.sh &
 bash downscaling_hadgem2-es_rcp4p5_2050-2099.sh &
 bash downscaling_ipsl-cm5a-lr_rcp4p5_2050-2099.sh & 
