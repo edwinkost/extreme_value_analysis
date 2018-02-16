a4d8bf093c8ca3a8d02addb66ade883ac739d277
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-02-16 12:11:58 +0100

    rcp8p5 - 2010-2049

---
+++ 2_downscaling_bias_corrected_climate_runs/rcp8p5/2010-2049/bash_jobs_for_downscaling_rcp8p5_2010-2049.sh
@@ -0,0 +1,8 @@
+
+bash downscaling_gfdl-esm2m_rcp8p5_2010-2049.sh &
+bash downscaling_hadgem2-es_rcp8p5_2010-2049.sh &
+bash downscaling_ipsl-cm5a-lr_rcp8p5_2010-2049.sh & 
+bash downscaling_miroc-esm-chem_rcp8p5_2010-2049.sh & 
+bash downscaling_noresm1-m_rcp8p5_2010-2049.sh &
+
+wait
