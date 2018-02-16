85d2909de49dba10792c6d4e9809871c3f350eb7
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-17 00:33:34 +0100

    rcp4p5 - 2050-2099 (2080)

---
+++ 2_downscaling_bias_corrected_climate_runs/etc-previous-runs/rcp6p0/inundation_downscaling/downscaling_flood_rcp6p0_gfdl-esm2m.sh
@@ -0,0 +1,74 @@
+#!/bin/bash                                                                                                                 
+#SBATCH -N 1                                                                                                                
+#SBATCH -t 119:00:00                                                                                                        
+#SBATCH -p normal                                                                                                           
+#SBATCH --constraint=haswell                                                                                                
+                                                                                                                            
+# mail alert at start, end and abortion of execution                                                                        
+#SBATCH --mail-type=ALL                                                                                                     
+                                                                                                                            
+# send mail to this address                                                                                                 
+#SBATCH --mail-user=edwinkost@gmail.com                                                                                     
+
+
+
+# move to the folder that contains the scripts
+cd /home/edwinsut/github/edwinkost/extreme_value_analysis
+
+
+################################################################################################
+#
+# Downscaling BIAS-CORRECTED floods - rcp6p0 - gfdl-esm2m - inunriver_rcp6p0_0000GFDL-ESM2M
+#
+################################################################################################
+#
+# 2010-2049 (2030)
+# - derive/downscale flood inundation maps at 30 arc-second resolution
+python 6_downscaling_parallel.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2010-2049/gfdl-esm2m/extreme_values/channel_storage/" /projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2010-2049/gfdl-esm2m/inundation_downscaled/bias_corrected/ "bias_corrected" channel_storage.map 6
+# - merging all downscaled map
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2010-2049/gfdl-esm2m/inundation_downscaled/bias_corrected/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2010-2049/gfdl-esm2m/inundation_downscaled/bias_corrected/" inunriver_rcp6p0_0000GFDL-ESM2M_2030 2010 2049 channel_storage.map 06
+#
+# 2030-2069 (2050)
+# - derive/downscale flood inundation maps at 30 arc-second resolution
+python 6_downscaling_parallel.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2030-2069/gfdl-esm2m/extreme_values/channel_storage/" /projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2030-2069/gfdl-esm2m/inundation_downscaled/bias_corrected/ "bias_corrected" channel_storage.map 6
+# - merging all downscaled map
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2030-2069/gfdl-esm2m/inundation_downscaled/bias_corrected/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2030-2069/gfdl-esm2m/inundation_downscaled/bias_corrected/" inunriver_rcp6p0_0000GFDL-ESM2M_2050 2030 2069 channel_storage.map 06
+#
+# 2050-2099 (2080)
+# - derive/downscale flood inundation maps at 30 arc-second resolution
+python 6_downscaling_parallel.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2050-2099/gfdl-esm2m/extreme_values/channel_storage/" /projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2050-2099/gfdl-esm2m/inundation_downscaled/bias_corrected/ "bias_corrected" channel_storage.map 6
+# - merging all downscaled map
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2050-2099/gfdl-esm2m/inundation_downscaled/bias_corrected/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2050-2099/gfdl-esm2m/inundation_downscaled/bias_corrected/" inunriver_rcp6p0_0000GFDL-ESM2M_2080 2050 2099 channel_storage.map 06
+#
+#
+################################################################################################
+
+
+
+################################################################################################
+#
+# Downscaling NON-bias_corrected floods - rcp6p0 - gfdl-esm2m - inunriver_rcp6p0_0000GFDL-ESM2M
+#
+################################################################################################
+#
+# 2010-2049 (2030)
+# - derive/downscale flood inundation maps at 30 arc-second resolution
+python 6_downscaling_parallel.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2010-2049/gfdl-esm2m/extreme_values/channel_storage/" /projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2010-2049/gfdl-esm2m/inundation_downscaled/including_bias/ "including_bias" channel_storage.map 6
+# - merging all downscaled map
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2010-2049/gfdl-esm2m/inundation_downscaled/including_bias/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2010-2049/gfdl-esm2m/inundation_downscaled/including_bias/" inunriver_rcp6p0_0000GFDL-ESM2M_2030 2010 2049 channel_storage.map 06
+#
+# 2030-2069 (2050)
+# - derive/downscale flood inundation maps at 30 arc-second resolution
+python 6_downscaling_parallel.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2030-2069/gfdl-esm2m/extreme_values/channel_storage/" /projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2030-2069/gfdl-esm2m/inundation_downscaled/including_bias/ "including_bias" channel_storage.map 6
+# - merging all downscaled map
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2030-2069/gfdl-esm2m/inundation_downscaled/including_bias/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2030-2069/gfdl-esm2m/inundation_downscaled/including_bias/" inunriver_rcp6p0_0000GFDL-ESM2M_2050 2030 2069 channel_storage.map 06
+#
+# 2050-2099 (2080)
+#~ # - derive/downscale flood inundation maps at 30 arc-second resolution
+python 6_downscaling_parallel.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2050-2099/gfdl-esm2m/extreme_values/channel_storage/" /projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2050-2099/gfdl-esm2m/inundation_downscaled/including_bias/ "including_bias" channel_storage.map 6
+# - merging all downscaled map
+python 7_merging_downscaled_maps_with_masking_out_and_following_name_convention.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2050-2099/gfdl-esm2m/inundation_downscaled/including_bias/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp6p0/2050-2099/gfdl-esm2m/inundation_downscaled/including_bias/" inunriver_rcp6p0_0000GFDL-ESM2M_2080 2050 2099 channel_storage.map 06
+#
+#
+################################################################################################
+
---
+++ 2_downscaling_bias_corrected_climate_runs/rcp4p5/2050-2099/bash_jobs_for_downscaling_rcp4p5_2050-2099.sh
@@ -0,0 +1,6 @@
+
+bash downscaling_gfdl-esm2m_rcp4p5_2050-2099.sh &
+bash downscaling_hadgem2-es_rcp4p5_2050-2099.sh &
+bash downscaling_ipsl-cm5a-lr_rcp4p5_2050-2099.sh & 
+bash downscaling_miroc-esm-chem_rcp4p5_2050-2099.sh & 
+bash downscaling_noresm1-m_rcp4p5_2050-2099.sh &
