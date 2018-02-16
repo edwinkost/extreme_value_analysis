7a3d2a205a374ea6d65fafb192b25afc6c3e29a3
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-16 22:22:58 +0100

    rcp8p5 - 2030-2069

---
+++ 2_downscaling_bias_corrected_climate_runs/rcp8p5/2030-2069/submit_jobs_for_downscaling_rcp8p5_2030-2069.sh
@@ -0,0 +1,6 @@
+
+sbatch downscaling_gfdl-esm2m_rcp8p5_2030-2069.sh 
+sbatch downscaling_hadgem2-es_rcp8p5_2030-2069.sh 
+sbatch downscaling_ipsl-cm5a-lr_rcp8p5_2030-2069.sh 
+sbatch downscaling_miroc-esm-chem_rcp8p5_2030-2069.sh 
+sbatch downscaling_noresm1-m_rcp8p5_2030-2069.sh
