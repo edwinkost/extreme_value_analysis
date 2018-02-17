60ef207e9445ac1b73947481da2bf8db6cb6f82e
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-17 23:38:24 +0100

    downscaling for rcp2p6 - all periods

--- 2_downscaling_bias_corrected_climate_runs/rcp2p6/bash_downscaling_rcp2p6_all_periods.sh
+++ 2_downscaling_bias_corrected_climate_runs/rcp2p6/bash_downscaling_rcp2p6_all_periods.sh
@@ -1,1 +1,8 @@
 
+cd /home/edwinsut/github/edwinkost/extreme_value_analysis/2_downscaling_bias_corrected_climate_runs/rcp2p6/
+
+bash 2010-2049/bash_jobs_for_downscaling_rcp2p6_2010-2049.sh
+bash 2030-2069/bash_jobs_for_downscaling_rcp2p6_2030-2069.sh
+bash 2050-2099/bash_jobs_for_downscaling_rcp2p6_2050-2099.sh
+
+
