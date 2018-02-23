f09cf1fe0cdf64fa6d45e4d296c8d97d6d3a5113
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-23 07:18:55 +0100

    We have to run for 2060-2099.

--- 0_analysis_for_rcp_runs_rerun_for_2060-2099/rcp4p5/extreme_value_analysis/extreme_value_analysis_rcp4p5_gfdl-esm2m.sh
+++ 0_analysis_for_rcp_runs_rerun_for_2060-2099/rcp4p5/extreme_value_analysis/extreme_value_analysis_rcp4p5_gfdl-esm2m.sh
@@ -6,30 +6,29 @@
 python 1a_get_maximum_events.py "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_gfdl-esm2m/no_correction/rcp4p5/merged_2006-2099/" 1 /scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/maximum_events/ 2060 2099 &
 python 1a_get_maximum_events.py "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_gfdl-esm2m/no_correction/rcp4p5/merged_2006-2099/" 2 /scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/maximum_events/ 2060 2099 &
 wait
-#~ #
-#~ #
-#~ #####################################################################################################
-#~ #
-#~ # derive hydro year type (based on the baseline/historical run using WATCH forcing data)
-#~ # DONE
-#~ #
-#~ #
-#~ #####################################################################################################
+#
+#
+#####################################################################################################
 
 
-#~ #####################################################################################################
-#~ #
-#~ # get annual maximum events based on a defined/given hydrological year tipe map
-#~ # 2010-2049 (2030)
-#~ # DONE
-#~ # 2030-2069 (2050)
-#~ # DONE
-#~ # 2050-2099 (2080)
-#~ python 2_merge_two_hydrological_year_result.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/maximum_events/" /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/hydrological_year/watch_1960-1999/hydrological_year_type.map "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/maximum_events/merged/" 2050 2099 &
-#~ wait
-#~ #
-#~ #
-#~ #####################################################################################################
+#####################################################################################################
+#
+# derive hydro year type (based on the baseline/historical run using WATCH forcing data)
+# DONE
+#
+#
+#####################################################################################################
+
+
+#####################################################################################################
+#
+# get annual maximum events based on a defined/given hydrological year tipe map
+# 2060-2099 (2080)
+python 2_merge_two_hydrological_year_result.py "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/maximum_events/" /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/hydrological_year/watch_1960-1999/hydrological_year_type.map "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/maximum_events/" 2060 2099 &
+wait
+#
+#
+#####################################################################################################
 
 
 #~ #####################################################################################################
