b832a28c6288b70244642d1f89e6377d9602da8c
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-23 16:56:31 +0100

    We have to run for 2060-2099.

--- 0_analysis_for_rcp_runs_rerun_for_2060-2099/rcp4p5/extreme_value_analysis/extreme_value_analysis_rcp4p5_gfdl-esm2m.sh
+++ 0_analysis_for_rcp_runs_rerun_for_2060-2099/rcp4p5/extreme_value_analysis/extreme_value_analysis_rcp4p5_gfdl-esm2m.sh
@@ -49,11 +49,11 @@ wait
 #
 # gumbel fits for the annual flood maxima variable 'channelStorage'
 # 2060-2099 (2080)
-python 4_gumbel_fits_get_gumbel_parameters.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/maximum_events/merged/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/maximum_events/surface_water_level_maximum/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/gumbel_fits/channel_storage/" 2050 2099 channelStorage &
+python 4_gumbel_fits_get_gumbel_parameters.py "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/maximum_events/merged/" "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/maximum_events/surface_water_level_maximum/" "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/gumbel_fits/channel_storage/" 2060 2099 channelStorage &
 # 
 # gumbel fits for the annual flood maxima variable 'surfaceWaterLevel'
 # 2060-2099 (2080)
-python 4_gumbel_fits_get_gumbel_parameters.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/maximum_events/merged/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/maximum_events/surface_water_level_maximum/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/gumbel_fits/surface_water_level/" 2050 2099 surfaceWaterLevel &
+python 4_gumbel_fits_get_gumbel_parameters.py "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/maximum_events/merged/" "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/maximum_events/surface_water_level_maximum/" "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/gumbel_fits/channel_storage/" 2060 2099 surfaceWaterLevel &
 # 
 wait
 # 
@@ -64,19 +64,16 @@ wait
 
 #####################################################################################################
 #
-#~ # apply gumbel parameters with bias correction, for the variable 'channelStorage' 
-#~ # 2010-2049 (2030)
-#~ # DONE
-#~ # 2030-2069 (2050)
-#~ # DONE
-#~ # 2050-2099 (2080)
-#~ python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/gumbel_fits/channel_storage/" "/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/gumbel_fits/gfdl-esm2m_1960-1999/" "/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/gumbel_fits/watch_1960-1999/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/extreme_values/channel_storage/" 2050 2099 None channelStorage
+# apply gumbel parameters with bias correction, for the variable 'channelStorage' 
+# 2060-2099 (2080)
+python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/gumbel_fits/channel_storage/" "/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/gumbel_fits/gfdl-esm2m_1960-1999/" "/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2016_12_11/flood_analyzer_analysis/historical/gumbel_fits/watch_1960-1999/" "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/extreme_values/channel_storage/" 2060 2099 None channelStorage &
 # 
 # gumbel fits for the annual flood maxima variable 'surfaceWaterLevel'
-#~ # 2010-2049 (2030)
-#~ python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2010-2049/gfdl-esm2m/gumbel_fits/surface_water_level/" "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/baseline_and_historical/gfdl-esm2m/1960-1999/gumbel_fits/" "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/baseline_and_historical/watch/1960-1999/gumbel_fits/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2010-2049/gfdl-esm2m/extreme_values/surface_water_level/" 2010 2049 surface_water_level_rcp4p5_0000GFDL-ESM2M_2030 surfaceWaterLevel &
-#~ # 2030-2069 (2050)
-#~ python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2030-2069/gfdl-esm2m/gumbel_fits/surface_water_level/" "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/baseline_and_historical/gfdl-esm2m/1960-1999/gumbel_fits/" "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/baseline_and_historical/watch/1960-1999/gumbel_fits/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2030-2069/gfdl-esm2m/extreme_values/surface_water_level/" 2030 2069 surface_water_level_rcp4p5_0000GFDL-ESM2M_2050 surfaceWaterLevel &
-#~ # 2050-2099 (2080)
-#~ python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/gumbel_fits/surface_water_level/" "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/baseline_and_historical/gfdl-esm2m/1960-1999/gumbel_fits/" "/scratch-shared/edwinsut/flood_analyzer_analysis_june_2017/baseline_and_historical/watch/1960-1999/gumbel_fits/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_june_2017/rcp4p5/2050-2099/gfdl-esm2m/extreme_values/surface_water_level/" 2050 2099 surface_water_level_rcp4p5_0000GFDL-ESM2M_2080 surfaceWaterLevel &
-#~ wait
+# 2060-2099 (2080)
+python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/gumbel_fits/surface_water_level/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_may_june_2017/historical/GFDL-ESM2M/1960-1999/gumbel_fits/" "/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysis_may_june_2017/historical/WATCH/1960-1999/gumbel_fits" "/scratch-shared/edwinsut/extreme_value_analysis_2018_02_XX_FIRST-TEST/rcp4p5/2060-2099/gfdl-esm2m/extreme_values/surface_water_level/" 2060 2099 surface_water_level_rcp4p5_0000GFDL-ESM2M_2080 surfaceWaterLevel &
+# 
+wait
+# 
+# 
+#####################################################################################################
+
