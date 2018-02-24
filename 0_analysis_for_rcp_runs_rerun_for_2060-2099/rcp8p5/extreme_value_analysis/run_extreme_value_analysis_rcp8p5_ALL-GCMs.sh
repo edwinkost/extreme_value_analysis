9add58bbe976465e49e59125e1540b97605a3050
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-24 03:29:11 +0100

    Start with rcp8p5 - ALL GCMs.

---
+++ 0_analysis_for_rcp_runs_rerun_for_2060-2099/rcp8p5/extreme_value_analysis/run_extreme_value_analysis_rcp8p5_ALL-GCMs.sh
@@ -0,0 +1,6 @@
+
+bash extreme_value_analysis_rcp8p5_gfdl-esm2m.sh 
+bash extreme_value_analysis_rcp8p5_hadgem2-es.sh 
+bash extreme_value_analysis_rcp8p5_ipsl-cm5a-lr.sh 
+bash extreme_value_analysis_rcp8p5_miroc-esm-chem.sh 
+bash extreme_value_analysis_rcp8p5_noresm1-m.sh
