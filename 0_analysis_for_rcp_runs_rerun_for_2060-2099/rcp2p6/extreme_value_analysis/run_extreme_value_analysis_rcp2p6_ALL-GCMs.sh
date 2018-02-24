8626cdb0f39ac9658b03ad15ea202663e700b599
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-24 03:55:59 +0100

    Start with rcp2p6 - ALL GCMs.

---
+++ 0_analysis_for_rcp_runs_rerun_for_2060-2099/rcp2p6/extreme_value_analysis/run_extreme_value_analysis_rcp2p6_ALL-GCMs.sh
@@ -0,0 +1,6 @@
+
+bash extreme_value_analysis_rcp2p6_gfdl-esm2m.sh 
+bash extreme_value_analysis_rcp2p6_hadgem2-es.sh 
+bash extreme_value_analysis_rcp2p6_ipsl-cm5a-lr.sh 
+bash extreme_value_analysis_rcp2p6_miroc-esm-chem.sh 
+bash extreme_value_analysis_rcp2p6_noresm1-m.sh
