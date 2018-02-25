2618dd463e3c3dce9357bf7fe422d08e2faa5473
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-25 17:16:16 +0100

    Start/redo downscaling rcp2p6 for the period 2060-2099.

---
+++ 2_downscaling_bias_corrected_climate_runs_rerun_for_2060-2099/rcp2p6/2060-2099/submit_jobs_for_downscaling_rcp2p6_2060-2099.sh
@@ -0,0 +1,6 @@
+
+sbatch downscaling_gfdl-esm2m_rcp2p6_2060-2099.sh 
+sbatch downscaling_hadgem2-es_rcp2p6_2060-2099.sh 
+sbatch downscaling_ipsl-cm5a-lr_rcp2p6_2060-2099.sh 
+sbatch downscaling_miroc-esm-chem_rcp2p6_2060-2099.sh 
+sbatch downscaling_noresm1-m_rcp2p6_2060-2099.sh
