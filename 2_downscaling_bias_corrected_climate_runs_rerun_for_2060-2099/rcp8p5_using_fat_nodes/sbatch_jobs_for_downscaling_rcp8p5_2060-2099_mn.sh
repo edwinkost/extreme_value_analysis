55ab265743571b5a2c0a97d47b8635495247410d
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-26 00:49:58 +0100

    Start/redo downscaling rcp6p0 for the period 2060-2099.

---
+++ 2_downscaling_bias_corrected_climate_runs_rerun_for_2060-2099/rcp8p5_using_fat_nodes/sbatch_jobs_for_downscaling_rcp8p5_2060-2099_mn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00
+#SBATCH -p fat
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+# send mail to this address
+#SBATCH --mail-user=edwinkost@gmail.com
+
+# job name
+#~ #SBATCH -J rcp8p5_2080_ghi
+#SBATCH -J rcp8p5_2080_mn
+
+#~ bash downscaling_gfdl-esm2m_rcp8p5_2060-2099.sh &
+#~ bash downscaling_hadgem2-es_rcp8p5_2060-2099.sh &
+#~ bash downscaling_ipsl-cm5a-lr_rcp8p5_2060-2099.sh & 
+bash downscaling_miroc-esm-chem_rcp8p5_2060-2099.sh & 
+bash downscaling_noresm1-m_rcp8p5_2060-2099.sh &
+
+wait
