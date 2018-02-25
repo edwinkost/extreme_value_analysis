d9a680bba5c515bf70691b887a74422656810de1
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-02-26 00:23:56 +0100

    Start/redo downscaling rcp4p5 for the period 2060-2099.

--- 2_downscaling_bias_corrected_climate_runs_rerun_for_2060-2099/rcp4p5_using_fat_nodes/2060-2099/sbatch_jobs_for_downscaling_rcp4p5_2060-2099_ghi.sh
+++ 2_downscaling_bias_corrected_climate_runs_rerun_for_2060-2099/rcp4p5_using_fat_nodes/2060-2099/sbatch_jobs_for_downscaling_rcp4p5_2060-2099_ghi.sh
@@ -2,13 +2,15 @@
 #SBATCH -N 1
 #SBATCH -t 119:59:00
 #SBATCH -p fat
-#SBATCH -J rcp4p5-2080-ghi
-#~ #SBATCH -J rcp4p5-2080-mn
 
 # mail alert at start, end and abortion of execution
 #SBATCH --mail-type=ALL
 # send mail to this address
 #SBATCH --mail-user=edwinkost@gmail.com
+
+# job name
+#SBATCH -J rcp4p5_2080_ghi
+#~ #SBATCH -J rcp4p5_2080_mn
 
 bash downscaling_gfdl-esm2m_rcp4p5_2060-2099.sh &
 bash downscaling_hadgem2-es_rcp4p5_2060-2099.sh &
