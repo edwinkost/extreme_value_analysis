2bfda53a39ffe0bdd7c31d0cdb4ec25e762d6817
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-05-28 13:00:35 +0200

    Checking the results of surface water level.

--- 0_bash_scripts_version_2018_05_XX/0_check_results/check_surface_water_level_results.sh
+++ 0_bash_scripts_version_2018_05_XX/0_check_results/check_surface_water_level_results.sh
@@ -18,6 +18,11 @@ set -x
 
 cd /home/edwinsut/github/edwinkost/extreme_value_analysis/
 
+echo historical
+cdo infon /scratch-shared/edwinsut/flood_analyzer_analysis_2018_05_XX/historical/1960-1999/GFDL-ESM2M/extreme_values/surface_water_level/*.nc
+python checking_results.py "/scratch-shared/edwinsut/flood_analyzer_analysis_2018_05_XX/historical/1960-1999/GFDL-ESM2M/extreme_values/surface_water_level/*mask*.map"
+
+
 echo rcp2p6
 cdo infon /scratch-shared/edwinhs/flood_analyzer_analysis_2018_05_XX/rcp2p6/*/*/extreme_values/surface_water_level/*.nc
 
