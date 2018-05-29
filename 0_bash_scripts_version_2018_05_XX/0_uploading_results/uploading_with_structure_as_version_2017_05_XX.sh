2404b083df59f5a088b8b854fe9e61ff74992afa
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-05-29 11:42:25 +0200

    Structuring the results version_2018_05_29 as 2017_05_XX

--- 0_bash_scripts_version_2018_05_XX/0_uploading_results/uploading_with_structure_as_version_2017_05_XX.sh
+++ 0_bash_scripts_version_2018_05_XX/0_uploading_results/uploading_with_structure_as_version_2017_05_XX.sh
@@ -1,33 +1,15 @@
 set -x 
 pwd
 
-RCP_CODE=rcp2p6
+TARGET_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2018_05_29_structured_as_version_2017_05_XX/
+mkdir ${TARGET_FOLDER}
 
-SOURCE_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2018_02_XX/river/inundation_30sec/${RCP_CODE}/
+# make the directory structure as (/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2018_02_XX_structured_as_version_2017_05_XX/)
+cd /projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2018_02_XX_structured_as_version_2017_05_XX/
+find . -type d > ${TARGET_FOLDER}//directory_structure_as_version_2017_05_XX.txt
+cd ${TARGET_FOLDER}
+xargs mkdir -p < dirs.txt
 
-# NOTE that we used the folder name 2050-2099 to store the values for the time period 2060-2099 (2080 as its mid year), see the file important_read_me_for_2050-2099.txt. 
-
-GCM_CODE=GFDL-ESM2M
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2010-2049/${GCM_CODE}/*.nc 2010-2049/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2030-2069/${GCM_CODE}/*.nc 2030-2069/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2060-2099/${GCM_CODE}/*.nc 2050-2099/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-
-GCM_CODE=HadGEM2-ES
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2010-2049/${GCM_CODE}/*.nc 2010-2049/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2030-2069/${GCM_CODE}/*.nc 2030-2069/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2060-2099/${GCM_CODE}/*.nc 2050-2099/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-
-GCM_CODE=IPSL-CM5A-LR
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2010-2049/${GCM_CODE}/*.nc 2010-2049/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2030-2069/${GCM_CODE}/*.nc 2030-2069/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2060-2099/${GCM_CODE}/*.nc 2050-2099/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-
-GCM_CODE=MIROC-ESM-CHEM
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2010-2049/${GCM_CODE}/*.nc 2010-2049/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2030-2069/${GCM_CODE}/*.nc 2030-2069/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2060-2099/${GCM_CODE}/*.nc 2050-2099/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-
-GCM_CODE=NorESM1-M
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2010-2049/${GCM_CODE}/*.nc 2010-2049/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2030-2069/${GCM_CODE}/*.nc 2030-2069/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
-rsync --progress -h -r --size-only ${SOURCE_FOLDER}/2060-2099/${GCM_CODE}/*.nc 2050-2099/inundation_downscaled_from_bias_corrected_floods/${GCM_CODE}/global/netcdf
+# uploading/making links for the rcp2p6
+cd /home/edwinsut/github/edwinkost/extreme_value_analysis/0_bash_scripts_version_2018_05_XX/0_uploading_results/
+bash uploading_with_structure_as_version_2017_05_XX_rcp2p6_inundation.sh
