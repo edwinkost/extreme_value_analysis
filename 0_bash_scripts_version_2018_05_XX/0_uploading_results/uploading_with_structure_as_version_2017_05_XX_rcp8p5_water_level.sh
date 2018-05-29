cdb78234561981c15cf77ab7c10e6b9fae8bf833
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2018-05-29 12:21:40 +0200

    Structuring the results version_2018_05_29 as 2017_05_XX

---
+++ 0_bash_scripts_version_2018_05_XX/0_uploading_results/uploading_with_structure_as_version_2017_05_XX_rcp8p5_water_level.sh
@@ -0,0 +1,23 @@
+set -x 
+pwd
+
+
+RCP_CODE=rcp8p5
+
+SOURCE_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2018_05_29/river/surface_water_level_05min/${RCP_CODE}/
+
+TARGET_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2018_05_29_structured_as_version_2017_05_XX/river/surface_water_level/${RCP_CODE}/
+cd ${TARGET_FOLDER}
+
+# remove the all folders within the TARGET_FOLDER as they will be replaced with links
+rm -r *
+
+# copy an important read me file
+cp -r /home/edwinsut/github/edwinkost/extreme_value_analysis/0_bash_scripts_version_2018_05_XX/0_uploading_results/important_read_me_for_2050-2099.txt ${TARGET_FOLDER}
+
+# NOTE that we used the link/folder "2050-2099" to store the values for the time period 2060-2099 (2080 as its mid year), see the file important_read_me_for_2050-2099.txt. 
+
+ln -s ${SOURCE_FOLDER}/2010-2049 2010-2049
+ln -s ${SOURCE_FOLDER}/2030-2069 2030-2069
+ln -s ${SOURCE_FOLDER}/2060-2099 2050-2099
+                                             
