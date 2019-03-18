55ca5be38dec326c4d04acc3b7fd87370d989e2c
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-03-18 13:53:38 +0100

    using edwin

--- 0_debug_2019_03_XX/post_processing_rcp8p5_2060-2099_gfdl-esm2m.sh
+++ 0_debug_2019_03_XX/post_processing_rcp8p5_2060-2099_gfdl-esm2m.sh
@@ -133,8 +133,8 @@ FUTURE_RCP_GCM_FOLDER=/projects/0/aqueduct/users/edwinsut/flood_analyzer_analysi
 # - with bias correction for the variables ['channelStorage'] - this should be without parallelization as this consumes huge memory
 python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py ${FUTURE_RCP_GCM_FOLDER}/channel_storage/ ${HISTORICAL_GCM_FOLDER}/channel_storage ${BASELINE_WATCH_FOLDER}/channel_storage/ ${MAIN_OUTPUT_FOLDER}/${RCP_CODE}/${STA_PERIOD}-${END_PERIOD}/${GCM_CAPITAL_LETTERS}/extreme_values/channel_storage/ ${STA_PERIOD} ${END_PERIOD} None channelStorage
 #
-#~ # - with bias correction for the variables ['surfaceWaterLevel'] - this should be without parallelization as this consumes huge memory
-#~ python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py ${FUTURE_RCP_GCM_FOLDER}/surface_water_level/ ${HISTORICAL_GCM_FOLDER}/surface_water_level ${BASELINE_WATCH_FOLDER}/surface_water_level/ ${MAIN_OUTPUT_FOLDER}/${RCP_CODE}/${STA_PERIOD}-${END_PERIOD}/${GCM_CAPITAL_LETTERS}/extreme_values/surface_water_level/ ${STA_PERIOD} ${END_PERIOD} surface_water_level_${RCP_CODE}_${GCM_CONVENTION_NAME} surfaceWaterLevel
+# - with bias correction for the variables ['surfaceWaterLevel'] - this should be without parallelization as this consumes huge memory
+python 5b_gumbel_fits_apply_gumbel_parameters_with_bias_correction_for_gcm_runs.py ${FUTURE_RCP_GCM_FOLDER}/surface_water_level/ ${HISTORICAL_GCM_FOLDER}/surface_water_level ${BASELINE_WATCH_FOLDER}/surface_water_level/ ${MAIN_OUTPUT_FOLDER}/${RCP_CODE}/${STA_PERIOD}-${END_PERIOD}/${GCM_CAPITAL_LETTERS}/extreme_values/surface_water_level/ ${STA_PERIOD} ${END_PERIOD} surface_water_level_${RCP_CODE}_${GCM_CONVENTION_NAME} surfaceWaterLevel
 #
 #
 ###################################################################################
