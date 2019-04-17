23927df00d372337e1de0e4275fbeb08961f19c7
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-04-17 14:24:37 +0200

    uploading historical inundation and surface water level results to the directory as structured in the version 2017_05_XX

---
+++ 0_bash_scripts_version_2019_03_XX/0_uploading_results/2_uploading_historical/uploading_with_structure_as_version_2017_05_XX_historical_inundation.sh
@@ -0,0 +1,14 @@
+set -x 
+pwd
+
+
+SOURCE_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2019_03_XX/river/inundation_30sec/1960-1999/WATCH
+
+TARGET_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2019_03_XX_structured_as_version_2017_05_XX/river/inundation/historical/inundation_downscaled/WATCH/1960-1999/global/
+cd ${TARGET_FOLDER}
+
+# remove the "netcdf" folder as it will be replaced with links
+rm -r netcdf
+
+# make the link
+ln -s ${SOURCE_FOLDER} netcdf
