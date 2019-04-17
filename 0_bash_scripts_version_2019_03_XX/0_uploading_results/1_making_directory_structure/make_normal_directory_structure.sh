3b6c9926f2076686e11c1d596891c585bc6d6325
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-04-17 13:38:52 +0200

    making the directory structure for uploading results

--- 0_bash_scripts_version_2019_03_XX/0_uploading_results/1_making_directory_structure/make_normal_directory_structure.sh
+++ 0_bash_scripts_version_2019_03_XX/0_uploading_results/1_making_directory_structure/make_normal_directory_structure.sh
@@ -4,6 +4,7 @@ set -x
 TARGET_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2019_03_XX/
 
 mkdir -p ${TARGET_FOLDER}
+
 cp -rf directory_structure.txt ${TARGET_FOLDER}
 cd ${TARGET_FOLDER}
 xargs mkdir -p < directory_structure.txt
