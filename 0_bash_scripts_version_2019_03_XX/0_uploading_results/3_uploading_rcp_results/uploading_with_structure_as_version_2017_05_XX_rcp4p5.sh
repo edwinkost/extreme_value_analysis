24256c50cc2bc9d43375dad6a4550c82a5e51489
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-04-17 15:27:49 +0200

    uploading_surface_water_level_rcp4p5_and_rcp8p5 to the directory structure as version 2017_05_XX, starting with rcp4p5

--- 0_bash_scripts_version_2019_03_XX/0_uploading_results/3_uploading_rcp_results/uploading_with_structure_as_version_2017_05_XX_rcp4p5.sh
+++ 0_bash_scripts_version_2019_03_XX/0_uploading_results/3_uploading_rcp_results/uploading_with_structure_as_version_2017_05_XX_rcp4p5.sh
@@ -4,10 +4,11 @@ pwd
 
 TARGET_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2019_03_XX_structured_as_version_2017_05_XX/
 
-# uploading/making links for the rcp4p5
 echo "rcp4p5"
+# uploading/making links for the rcp4p5
 bash uploading_with_structure_as_version_2017_05_XX_rcp4p5_inundation.sh
 bash uploading_with_structure_as_version_2017_05_XX_rcp4p5_water_level.sh
 
 set +x
+
 
