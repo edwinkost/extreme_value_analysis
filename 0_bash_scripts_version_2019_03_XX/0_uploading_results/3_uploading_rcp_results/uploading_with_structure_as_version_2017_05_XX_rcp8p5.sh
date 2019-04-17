5a52844a9039cb0c2ed050463a991440601bc106
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-04-17 15:37:03 +0200

    uploading to the directory structure as version 2017_05_XX, rcp8p5

---
+++ 0_bash_scripts_version_2019_03_XX/0_uploading_results/3_uploading_rcp_results/uploading_with_structure_as_version_2017_05_XX_rcp8p5.sh
@@ -0,0 +1,14 @@
+
+set -x 
+pwd
+
+TARGET_FOLDER=/projects/0/aqueduct/users/edwinsut/aqueduct_flood_analyzer_results/version_2019_03_XX_structured_as_version_2017_05_XX/
+
+echo "rcp8p5"
+# uploading/making links for the rcp8p5
+bash uploading_with_structure_as_version_2017_05_XX_rcp8p5_inundation.sh
+bash uploading_with_structure_as_version_2017_05_XX_rcp8p5_water_level.sh
+
+set +x
+
+
